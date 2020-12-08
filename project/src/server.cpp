ee940164109a6636efbc3356dfef662c3671bdd8
s950449 <950449@hotmail.com.tw>
2020-12-08 13:28:38 +0800

    [Project] Update server

--- project/src/server.cpp
+++ project/src/server.cpp
@@ -6,6 +6,14 @@
 int main(int argc,char** argv){
     HTTPD server;
     signal(SIGINT,SignalHandler);
+#ifndef LOCAL
+    std::string pub("/etc/letsencrypt/archive/sim2.csie.org/cert1.pem");
+    std::string pri("/etc/letsencrypt/archive/sim2.csie.org/privkey.pem");
+#else
+    std::string pub("/tmp/ssl/cert.pem");
+    std::string pri("/tmp/ssl/key.pem");   
+#endif
+    server.init_key(pub,pri);
     std::ifstream config_file("./config");
     pid_t PID;
     PID=fork();
